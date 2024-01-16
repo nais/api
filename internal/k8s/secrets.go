fa792040f195b506e80c5dc73cc82b9efe7c0850
Trong Huu Nguyen <trong.huu.nguyen@nav.no>
2024-01-16 09:58:33 +0100

    secrets: naive implementation of k8s client

--- internal/k8s/secrets.go
+++ internal/k8s/secrets.go
@@ -5,32 +5,35 @@ import (
 
 	"github.com/nais/api/internal/graph/model"
 	"github.com/nais/api/internal/graph/scalar"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/labels"
+	"k8s.io/apimachinery/pkg/selection"
+)
+
+const (
+	consoleSecretLabelKey = "nais.io/managed-by"
+	consoleSecretLabelVal = "console"
 )
 
 // TODO: implement impersonation
 func (c *Client) Secrets(ctx context.Context, team string) ([]*model.Secret, error) {
 	ret := make([]*model.Secret, 0)
-	relevantSecretLabels := labels.SelectorFromSet(map[string]string{
-		"nais.io/managed-by": "console",
-	})
+
+	secretReq, err := labels.NewRequirement(consoleSecretLabelKey, selection.Equals, []string{consoleSecretLabelVal})
+	if err != nil {
+		return nil, c.error(ctx, err, "creating label selector")
+	}
+	secretSelector := labels.NewSelector().Add(*secretReq)
 
 	for env, infs := range c.informers {
-		objs, err := infs.SecretInformer.Lister().Secrets(team).List(relevantSecretLabels)
+		objs, err := infs.SecretInformer.Lister().Secrets(team).List(secretSelector)
 		if err != nil {
 			return nil, c.error(ctx, err, "listing applications")
 		}
 
 		for _, obj := range objs {
-			secret := &model.Secret{
-				ID: makeSecretIdent(env, obj.GetNamespace(), obj.GetName()),
-				Env: model.Env{
-					Name: env,
-				},
-				Name: obj.Name,
-				Data: secretBytesToString(obj.Data),
-			}
-			ret = append(ret, secret)
+			ret = append(ret, toGraphSecret(obj, env))
 		}
 	}
 	return ret, nil
@@ -42,14 +45,61 @@ func (c *Client) Secret(ctx context.Context, name, team, env string) (*model.Sec
 		return nil, c.error(ctx, err, "getting secret")
 	}
 
+	return toGraphSecret(secret, env), nil
+}
+
+func (c *Client) CreateSecret(ctx context.Context, secret *model.Secret) error {
+	env := secret.Env.Name
+	namespace := secret.GQLVars.Team.String()
+	_, err := c.clientSets[env].CoreV1().Secrets(namespace).Create(ctx, toKubeSecret(secret), metav1.CreateOptions{})
+	if err != nil {
+		return c.error(ctx, err, "creating secret")
+	}
+	return nil
+}
+
+func (c *Client) UpdateSecret(ctx context.Context, secret *model.Secret) error {
+	env := secret.Env.Name
+	namespace := secret.GQLVars.Team.String()
+	_, err := c.clientSets[env].CoreV1().Secrets(namespace).Update(ctx, toKubeSecret(secret), metav1.UpdateOptions{})
+	if err != nil {
+		return c.error(ctx, err, "updating secret")
+	}
+	return nil
+}
+
+func (c *Client) DeleteSecret(ctx context.Context, secret *model.Secret) error {
+	env := secret.Env.Name
+	namespace := secret.GQLVars.Team.String()
+	err := c.clientSets[env].CoreV1().Secrets(namespace).Delete(ctx, secret.Name, metav1.DeleteOptions{})
+	if err != nil {
+		return c.error(ctx, err, "deleting secret")
+	}
+	return nil
+}
+
+func toKubeSecret(secret *model.Secret) *corev1.Secret {
+	return &corev1.Secret{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      secret.Name,
+			Namespace: secret.GQLVars.Team.String(),
+			Labels: map[string]string{
+				consoleSecretLabelKey: consoleSecretLabelVal,
+			},
+		},
+		StringData: secret.Data,
+	}
+}
+
+func toGraphSecret(obj *corev1.Secret, env string) *model.Secret {
 	return &model.Secret{
-		ID: makeSecretIdent(env, secret.GetNamespace(), secret.GetName()),
+		ID: makeSecretIdent(env, obj.GetNamespace(), obj.GetName()),
 		Env: model.Env{
 			Name: env,
 		},
-		Name: secret.Name,
-		Data: secretBytesToString(secret.Data),
-	}, nil
+		Name: obj.Name,
+		Data: secretBytesToString(obj.Data),
+	}
 }
 
 func secretBytesToString(data map[string][]byte) map[string]string {
@@ -63,9 +113,3 @@ func secretBytesToString(data map[string][]byte) map[string]string {
 func makeSecretIdent(env, namespace, name string) scalar.Ident {
 	return scalar.SecretIdent("secret_" + env + "_" + namespace + "_" + name)
 }
-
-func (c *Client) CreateSecret(ctx context.Context, secret *model.Secret) error { return nil }
-
-func (c *Client) UpdateSecret(ctx context.Context, secret *model.Secret) error { return nil }
-
-func (c *Client) DeleteSecret(ctx context.Context, secret *model.Secret) error { return nil }
