9a475c44ee18542dce2e15e768a6afdd747e6594
Roger Bjørnstad <roger.bjornstad@nav.no>
2024-02-01 14:08:39 +0100

    Add vulnerability-related database tables
    Co-authored-by: ybelMekk <youssef.bel.mekki@nav.no>

---
+++ internal/database/migrations/0002_vulnerabilities.sql
@@ -0,0 +1,23 @@
+CREATE TABLE dependencytrack_projects {
+    id serial PRIMARY KEY,
+    environment text NOT NULL,
+    team_slug slug NOT NULL,
+    app text NOT NULL,
+    projectId uuid NOT NULL,
+    CONSTRAINT dependencytrack_projects_key UNIQUE (environment, team_slug, app, projectId)
+};
+
+CREATE TABLE vulnerability_metrics (
+    date date NOT NULL,
+    dependencytrack_project_id serial NOT NULL,
+    critical integer NOT NULL,
+    high integer NOT NULL,
+    medium integer NOT NULL,
+    low integer NOT NULL,
+    unassigned integer NOT NULL,
+    risk_score double precision NOT NULL,
+    CONSTRAINT vulnerability_metrics_key KEY (date, dependencytrack_project_id),
+    CONSTRAINT fk_vulnerability_metrics_dependencytrack_project_id
+        FOREIGN KEY (dependencytrack_project_id)
+            REFERENCES dependencytrack_projects (id) ON DELETE CASCADE,
+);
