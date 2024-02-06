f6d5ff950237fef406ce890682ecf641dcfac71f
Thomas Krampl <thomas.siegfried.krampl@nav.no>
2024-02-06 18:40:56 +0100

    Hopefully the mockclient is better now

--- pkg/apiclient/mockclient.go
+++ pkg/apiclient/mockclient.go
@@ -6,7 +6,6 @@ import (
 	"errors"
 	"fmt"
 	"net"
-	"sync"
 	"testing"
 
 	"github.com/nais/api/pkg/protoapi"
@@ -17,15 +16,14 @@ import (
 
 type TestingHelpers struct {
 	testing.TB
-	cancels map[*context.CancelFunc]struct{}
-	buf     bytes.Buffer
-
-	lock      sync.Mutex
-	hasFailed bool
+	cancels  map[*context.CancelFunc]struct{}
+	buf      bytes.Buffer
+	shutdown func()
 }
 
 func (t *TestingHelpers) printBuffer() {
-	if t.buf.Len() > 0 {
+	if t.buf.Len() > 0 && t.Failed() {
+		fmt.Println(t.Name())
 		fmt.Println(t.buf.String())
 	}
 
@@ -34,20 +32,29 @@ func (t *TestingHelpers) printBuffer() {
 	}
 }
 
-func (t *TestingHelpers) FailNow() {
-	// 	// t.lock.Lock()
-	// 	// defer t.lock.Unlock()
-	// 	// if t.hasFailed {
-	// 	// 	return
-	// 	// }
+func (t *TestingHelpers) Errorf(format string, args ...interface{}) {
+	fmt.Fprintf(&t.buf, format+"\n", args...)
+	t.TB.Errorf(format, args...)
+}
 
-	// 	// for conn := range t.cancels {
-	// 	// 	(*conn)()
-	// 	// }
-	// 	// fmt.Print("Fail in the following test:", t.t.Name())
-	// 	// fmt.Println(t.buf.String())
-	// 	// t.buf.Reset()
+func (t *TestingHelpers) Logf(format string, args ...interface{}) {
+	fmt.Fprintf(&t.buf, format+"\n", args...)
+	t.TB.Logf(format, args...)
+}
+
+func (t *TestingHelpers) Fail() {
 	t.TB.Fail()
+	t.shutdown()
+}
+
+func (t *TestingHelpers) FailNow() {
+	if t.buf.Len() > 0 {
+		fmt.Print("Fail in the following test:", t.Name())
+		fmt.Println(t.buf.String())
+	}
+	t.buf.Reset()
+	t.shutdown()
+	t.Fail()
 	panic("")
 }
 
@@ -64,10 +71,12 @@ func NewMockClient(t testing.TB) (*APIClient, *MockServers) {
 
 	s := grpc.NewServer()
 
+	ctx, shutdown := context.WithCancel(context.Background())
 	th := &TestingHelpers{
-		TB:      t,
-		cancels: map[*context.CancelFunc]struct{}{},
-		buf:     bytes.Buffer{},
+		TB:       t,
+		cancels:  map[*context.CancelFunc]struct{}{},
+		buf:      bytes.Buffer{},
+		shutdown: shutdown,
 	}
 	th.Cleanup(th.printBuffer)
 	mockServers := &MockServers{
@@ -85,9 +94,7 @@ func NewMockClient(t testing.TB) (*APIClient, *MockServers) {
 	protoapi.RegisterUsersServer(s, mockServers.Users)
 
 	listener := bufconn.Listen(1024 * 1024)
-	dialer := func(ctx context.Context, s string) (net.Conn, error) {
-		ctx, cancel := context.WithCancel(ctx)
-		th.cancels[&cancel] = struct{}{}
+	dialer := func(_ context.Context, s string) (net.Conn, error) {
 		return listener.DialContext(ctx)
 	}
 
@@ -95,6 +102,11 @@ func NewMockClient(t testing.TB) (*APIClient, *MockServers) {
 	if err != nil {
 		t.Fatal(err)
 	}
+
+	go func() {
+		<-ctx.Done()
+		s.Stop()
+	}()
 
 	go func() {
 		if err := s.Serve(listener); err != nil {
@@ -110,7 +122,7 @@ func NewMockClient(t testing.TB) (*APIClient, *MockServers) {
 	}()
 
 	t.Cleanup(func() {
-		s.GracefulStop()
+		s.Stop()
 	})
 
 	return client, mockServers
