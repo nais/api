24c5731d3f371a3bc5fef5a35f268bf6f780d123
Roger Bjørnstad <roger.bjornstad@nav.no>
2024-02-07 09:28:47 +0100

    Refactor resolver types in app, github_repo, and users
    Co-authored-by: ybelMekk <youssef.bel.mekki@nav.no>

--- internal/graph/github_repo.resolvers.go
+++ internal/graph/github_repo.resolvers.go
@@ -104,5 +104,7 @@ func (r *Resolver) GitHubRepository() gengql.GitHubRepositoryResolver {
 // Mutation returns gengql.MutationResolver implementation.
 func (r *Resolver) Mutation() gengql.MutationResolver { return &mutationResolver{r} }
 
-type gitHubRepositoryResolver struct{ *Resolver }
-type mutationResolver struct{ *Resolver }
+type (
+	gitHubRepositoryResolver struct{ *Resolver }
+	mutationResolver         struct{ *Resolver }
+)
