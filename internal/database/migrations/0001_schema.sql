dade1c094d788cf98a7bfd6f334a84dbda7c25df
Christer Edvartsen <christer.edvartsen@nav.no>
2024-02-15 10:27:18 +0100

    squash migrations

--- internal/database/migrations/0001_schema.sql
+++ internal/database/migrations/0001_schema.sql
@@ -15,9 +15,11 @@ $$;
 -- +goose StatementEnd
 
 -- extensions
+
 CREATE EXTENSION fuzzystrmatch;
 
 -- functions
+
 CREATE OR REPLACE FUNCTION set_updated_at() RETURNS TRIGGER AS
 $$ BEGIN NEW.updated_at = NOW(); RETURN NEW; END; $$
     LANGUAGE plpgsql;
@@ -50,17 +52,15 @@ CREATE TYPE repository_authorization_enum AS ENUM (
     'deploy'
 );
 
-
 -- tables
 
 CREATE TABLE api_keys (
-    api_key text NOT NULL,
-    service_account_id uuid NOT NULL,
-    PRIMARY KEY(api_key)
+    api_key text PRIMARY KEY,
+    service_account_id uuid NOT NULL
 );
 
 CREATE TABLE audit_logs (
-    id uuid DEFAULT gen_random_uuid() NOT NULL,
+    id uuid DEFAULT gen_random_uuid() PRIMARY KEY,
     created_at timestamp with time zone DEFAULT CURRENT_TIMESTAMP NOT NULL,
     correlation_id uuid NOT NULL,
     component_name text NOT NULL,
@@ -68,8 +68,7 @@ CREATE TABLE audit_logs (
     action text NOT NULL,
     message text NOT NULL,
     target_type text NOT NULL,
-    target_identifier text NOT NULL,
-    PRIMARY KEY(id)
+    target_identifier text NOT NULL
 );
 
 CREATE TABLE cost (
@@ -83,21 +82,27 @@ CREATE TABLE cost (
     CONSTRAINT daily_cost_key UNIQUE (environment, team_slug, app, cost_type, date)
 );
 
-CREATE TABLE environments (
-    name text NOT NULL PRIMARY KEY,
-    gcp boolean DEFAULT false NOT NULL
+CREATE TABLE dependencytrack_projects(
+    id uuid PRIMARY KEY,
+    environment text NOT NULL,
+    team_slug slug NOT NULL,
+    app text NOT NULL,
+    created_at timestamp with time zone DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
 
+CREATE TABLE environments (
+    name text PRIMARY KEY,
+    gcp boolean DEFAULT false NOT NULL
+);
 COMMENT ON TABLE environments IS 'This table is used to store the environments that are available in the system. It will be emptied and repopulated when the system starts.';
 
 CREATE TABLE reconciler_errors (
-    id BIGSERIAL,
+    id BIGSERIAL PRIMARY KEY,
     correlation_id uuid NOT NULL,
     reconciler text NOT NULL,
     created_at timestamp with time zone DEFAULT CURRENT_TIMESTAMP NOT NULL,
     error_message text NOT NULL,
     team_slug slug NOT NULL,
-    PRIMARY KEY(id),
     UNIQUE (team_slug, reconciler)
 );
 
@@ -115,36 +120,32 @@ CREATE TABLE reconciler_opt_outs (
     team_slug slug NOT NULL,
     user_id UUID NOT NULL,
     reconciler_name text NOT NULL,
-    PRIMARY KEY(team_slug, user_id, reconciler_name)
+    PRIMARY KEY (team_slug, user_id, reconciler_name)
 );
 
-CREATE TABLE reconciler_resources (
-  id UUID DEFAULT gen_random_uuid() NOT NULL PRIMARY KEY,
-  reconciler_name text NOT NULL,
-  team_slug slug NOT NULL,
-  name TEXT NOT NULL,
-  value bytea NOT NULL,
-  metadata JSONB DEFAULT '{}'::jsonb NOT NULL,
-  created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP NOT NULL,
-  updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-  UNIQUE(reconciler_name, team_slug, name)
+CREATE TABLE reconciler_states (
+    id UUID DEFAULT gen_random_uuid() NOT NULL PRIMARY KEY,
+    reconciler_name text NOT NULL,
+    team_slug slug NOT NULL,
+    value bytea NOT NULL,
+    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    UNIQUE (reconciler_name, team_slug)
 );
 
 CREATE TABLE reconcilers (
-    name text NOT NULL,
-    display_name text NOT NULL,
+    name text PRIMARY KEY,
+    display_name text NOT NULL UNIQUE,
     description text NOT NULL,
     enabled boolean DEFAULT false NOT NULL,
-    member_aware boolean DEFAULT false NOT NULL,
-    PRIMARY KEY(name),
-    UNIQUE(display_name)
+    member_aware boolean DEFAULT false NOT NULL
 );
 
 CREATE TABLE repository_authorizations (
     team_slug slug NOT NULL,
     github_repository text NOT NULL,
     repository_authorization repository_authorization_enum NOT NULL,
-    PRIMARY KEY(team_slug, github_repository, repository_authorization)
+    PRIMARY KEY (team_slug, github_repository, repository_authorization)
 );
 
 CREATE TABLE resource_utilization_metrics (
@@ -162,40 +163,35 @@ CREATE TABLE resource_utilization_metrics (
 );
 
 CREATE TABLE service_account_roles (
-    id SERIAL,
+    id SERIAL PRIMARY KEY,
     role_name role_name NOT NULL,
     service_account_id uuid NOT NULL,
     target_team_slug slug,
     target_service_account_id uuid,
-    PRIMARY KEY(id),
     CHECK (((target_team_slug IS NULL) OR (target_service_account_id IS NULL)))
 );
 
 CREATE TABLE service_accounts (
-    id uuid DEFAULT gen_random_uuid() NOT NULL,
-    name text NOT NULL,
-    PRIMARY KEY(id),
-    UNIQUE(name)
+    id uuid DEFAULT gen_random_uuid() PRIMARY KEY,
+    name text NOT NULL UNIQUE
 );
 
 CREATE TABLE sessions (
-    id uuid DEFAULT gen_random_uuid() NOT NULL,
+    id uuid DEFAULT gen_random_uuid() PRIMARY KEY,
     user_id uuid NOT NULL,
-    expires timestamp with time zone NOT NULL,
-    PRIMARY KEY(id)
+    expires timestamp with time zone NOT NULL
 );
 
 CREATE TABLE team_delete_keys (
-    key uuid DEFAULT gen_random_uuid() NOT NULL,
+    key uuid DEFAULT gen_random_uuid() PRIMARY KEY,
     team_slug slug NOT NULL,
     created_at timestamp with time zone DEFAULT CURRENT_TIMESTAMP NOT NULL,
     created_by uuid NOT NULL,
-    confirmed_at timestamp with time zone,
-    PRIMARY KEY(key)
+    confirmed_at timestamp with time zone
 );
 
 CREATE TABLE teams (
-    slug slug NOT NULL,
+    slug slug PRIMARY KEY,
     purpose text NOT NULL,
     last_successful_sync timestamp without time zone,
     slack_channel text NOT NULL,
@@ -203,39 +199,47 @@ CREATE TABLE teams (
     azure_group_id uuid,
     github_team_slug text,
     gar_repository text,
-    PRIMARY KEY(slug),
     CHECK ((TRIM(BOTH FROM purpose) <> ''::text)),
     CHECK ((slack_channel ~ '^#[a-z0-9æøå_-]{2,80}$'::text))
 );
 
 CREATE TABLE team_environments (
-    id uuid DEFAULT gen_random_uuid() NOT NULL,
+    id uuid DEFAULT gen_random_uuid() PRIMARY KEY,
     team_slug slug NOT NULL,
     environment text NOT NULL,
     slack_alerts_channel text,
     gcp_project_id text,
-    UNIQUE(team_slug, environment),
+    UNIQUE (team_slug, environment),
     CHECK ((slack_alerts_channel IS NULL OR slack_alerts_channel ~ '^#[a-z0-9æøå_-]{2,80}$'::text))
 );
 
 CREATE TABLE user_roles (
-    id SERIAL,
+    id SERIAL PRIMARY KEY,
     role_name role_name NOT NULL,
     user_id uuid NOT NULL,
     target_team_slug slug,
     target_service_account_id uuid,
-    PRIMARY KEY(id),
     CHECK (((target_team_slug IS NULL) OR (target_service_account_id IS NULL)))
 );
 
 CREATE TABLE users (
-    id uuid DEFAULT gen_random_uuid() NOT NULL,
-    email text NOT NULL,
+    id uuid DEFAULT gen_random_uuid() PRIMARY KEY,
+    email text NOT NULL UNIQUE,
     name text NOT NULL,
-    external_id text NOT NULL,
-    PRIMARY KEY(id),
-    UNIQUE(email),
-    UNIQUE(external_id)
+    external_id text NOT NULL UNIQUE
+);
+
+CREATE TABLE vulnerability_metrics(
+    id uuid DEFAULT gen_random_uuid() PRIMARY KEY,
+    date date NOT NULL,
+    dependencytrack_project_id uuid NOT NULL,
+    critical integer NOT NULL,
+    high integer NOT NULL,
+    medium integer NOT NULL,
+    low integer NOT NULL,
+    unassigned integer NOT NULL,
+    risk_score double precision NOT NULL,
+    CONSTRAINT vulnerability_metric UNIQUE (date, dependencytrack_project_id)
 );
 
 -- views
@@ -259,74 +263,81 @@ CREATE VIEW team_all_environments AS (
 -- additional indexes
 
 CREATE INDEX ON audit_logs USING btree (created_at DESC);
-CREATE INDEX cost_env_idx ON cost (environment);
-CREATE INDEX cost_team_idx ON cost (team_slug);
-CREATE INDEX cost_app_idx ON cost (app);
-CREATE INDEX cost_date_idx ON cost (date);
+
+CREATE INDEX ON cost (environment);
+CREATE INDEX ON cost (team_slug);
+CREATE INDEX ON cost (app);
+CREATE INDEX ON cost (date);
+
 CREATE INDEX ON reconciler_errors USING btree (created_at DESC);
 CREATE INDEX ON resource_utilization_metrics (app);
 CREATE INDEX ON resource_utilization_metrics (environment);
 CREATE INDEX ON resource_utilization_metrics (resource_type);
 CREATE INDEX ON resource_utilization_metrics (team_slug);
 CREATE INDEX ON resource_utilization_metrics (timestamp);
-CREATE INDEX ON reconciler_resources (reconciler_name, name, team_slug);
+
 CREATE UNIQUE INDEX ON service_account_roles USING btree (service_account_id, role_name) WHERE ((target_team_slug IS NULL) AND (target_service_account_id IS NULL));
-CREATE UNIQUE INDEX ON user_roles USING btree (user_id, role_name) WHERE ((target_team_slug IS NULL) AND (target_service_account_id IS NULL));
 CREATE UNIQUE INDEX ON service_account_roles USING btree (service_account_id, role_name, target_service_account_id) WHERE (target_service_account_id IS NOT NULL);
-CREATE UNIQUE INDEX ON user_roles USING btree (user_id, role_name, target_service_account_id) WHERE (target_service_account_id IS NOT NULL);
 CREATE UNIQUE INDEX ON service_account_roles USING btree (service_account_id, role_name, target_team_slug) WHERE (target_team_slug IS NOT NULL);
+
+CREATE UNIQUE INDEX ON user_roles USING btree (user_id, role_name) WHERE ((target_team_slug IS NULL) AND (target_service_account_id IS NULL));
+CREATE UNIQUE INDEX ON user_roles USING btree (user_id, role_name, target_service_account_id) WHERE (target_service_account_id IS NOT NULL);
 CREATE UNIQUE INDEX ON user_roles USING btree (user_id, role_name, target_team_slug) WHERE (target_team_slug IS NOT NULL);
 
 -- foreign keys
 
 ALTER TABLE api_keys
-ADD FOREIGN KEY (service_account_id) REFERENCES service_accounts(id) ON DELETE CASCADE;
+    ADD FOREIGN KEY (service_account_id) REFERENCES service_accounts(id) ON DELETE CASCADE;
 
-ALTER TABLE reconciler_config
-ADD FOREIGN KEY (reconciler) REFERENCES reconcilers(name) ON DELETE CASCADE;
-
-ALTER TABLE reconciler_errors
-ADD FOREIGN KEY (reconciler) REFERENCES reconcilers(name) ON DELETE CASCADE,
-ADD FOREIGN KEY (team_slug) REFERENCES teams(slug) ON DELETE CASCADE;
-
-ALTER TABLE reconciler_opt_outs
-ADD FOREIGN KEY (team_slug) REFERENCES teams(slug) ON DELETE CASCADE,
-ADD FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
-ADD FOREIGN KEY (reconciler_name) REFERENCES reconcilers(name) ON DELETE CASCADE;
+ALTER TABLE dependencytrack_projects
+    ADD FOREIGN KEY (team_slug) REFERENCES teams(slug) ON DELETE CASCADE;
 
 ALTER TABLE repository_authorizations
     ADD FOREIGN KEY (team_slug) REFERENCES teams(slug) ON DELETE CASCADE;
 
-ALTER TABLE service_account_roles
-ADD FOREIGN KEY (service_account_id) REFERENCES service_accounts(id) ON DELETE CASCADE,
-ADD FOREIGN KEY (target_service_account_id) REFERENCES service_accounts(id) ON DELETE CASCADE,
-ADD FOREIGN KEY (target_team_slug) REFERENCES teams(slug) ON DELETE CASCADE;
+ALTER TABLE reconciler_config
+    ADD FOREIGN KEY (reconciler) REFERENCES reconcilers(name) ON DELETE CASCADE;
 
-ALTER TABLE sessions
-ADD FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE;
+ALTER TABLE reconciler_errors
+    ADD FOREIGN KEY (reconciler) REFERENCES reconcilers(name) ON DELETE CASCADE,
+    ADD FOREIGN KEY (team_slug) REFERENCES teams(slug) ON DELETE CASCADE;
 
-ALTER TABLE team_delete_keys
-ADD FOREIGN KEY (created_by) REFERENCES users(id) ON DELETE CASCADE,
-ADD FOREIGN KEY (team_slug) REFERENCES teams(slug) ON DELETE CASCADE;
+ALTER TABLE reconciler_opt_outs
+    ADD FOREIGN KEY (team_slug) REFERENCES teams(slug) ON DELETE CASCADE,
+    ADD FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+    ADD FOREIGN KEY (reconciler_name) REFERENCES reconcilers(name) ON DELETE CASCADE;
 
-ALTER TABLE team_environments
-ADD FOREIGN KEY (team_slug) REFERENCES teams(slug) ON DELETE CASCADE;
-
-ALTER TABLE user_roles
-ADD FOREIGN KEY (target_service_account_id) REFERENCES service_accounts(id) ON DELETE CASCADE,
-ADD FOREIGN KEY (target_team_slug) REFERENCES teams(slug) ON DELETE CASCADE,
-ADD FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE;
+ALTER TABLE reconciler_states
+    ADD FOREIGN KEY (reconciler_name) REFERENCES reconcilers(name) ON DELETE CASCADE,
+    ADD FOREIGN KEY (team_slug) REFERENCES teams(slug) ON DELETE CASCADE;
 
 ALTER TABLE resource_utilization_metrics
-ADD FOREIGN KEY (team_slug) REFERENCES teams(slug) ON DELETE CASCADE;
+    ADD FOREIGN KEY (team_slug) REFERENCES teams(slug) ON DELETE CASCADE;
 
-ALTER TABLE reconciler_resources
-ADD FOREIGN KEY (team_slug) REFERENCES teams(slug) ON DELETE CASCADE,
-ADD FOREIGN KEY (reconciler_name) REFERENCES reconcilers(name) ON DELETE CASCADE;
+ALTER TABLE service_account_roles
+    ADD FOREIGN KEY (service_account_id) REFERENCES service_accounts(id) ON DELETE CASCADE,
+    ADD FOREIGN KEY (target_service_account_id) REFERENCES service_accounts(id) ON DELETE CASCADE,
+    ADD FOREIGN KEY (target_team_slug) REFERENCES teams(slug) ON DELETE CASCADE;
+
+ALTER TABLE sessions
+    ADD FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE;
+
+ALTER TABLE team_delete_keys
+    ADD FOREIGN KEY (created_by) REFERENCES users(id) ON DELETE CASCADE,
+    ADD FOREIGN KEY (team_slug) REFERENCES teams(slug) ON DELETE CASCADE;
+
+ALTER TABLE team_environments
+    ADD FOREIGN KEY (team_slug) REFERENCES teams(slug) ON DELETE CASCADE;
+
+ALTER TABLE user_roles
+    ADD FOREIGN KEY (target_service_account_id) REFERENCES service_accounts(id) ON DELETE CASCADE,
+    ADD FOREIGN KEY (target_team_slug) REFERENCES teams(slug) ON DELETE CASCADE,
+    ADD FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE;
+
+ALTER TABLE vulnerability_metrics
+    ADD FOREIGN KEY (dependencytrack_project_id) REFERENCES dependencytrack_projects(id) ON DELETE CASCADE;
 
 -- triggers
-CREATE TRIGGER reconciler_resources_set_updated
-    BEFORE UPDATE
-    ON reconciler_resources
-    FOR EACH ROW
-    EXECUTE PROCEDURE set_updated_at();
+CREATE TRIGGER reconciler_states_set_updated BEFORE
+    UPDATE
+    ON reconciler_states FOR EACH ROW EXECUTE PROCEDURE set_updated_at();
