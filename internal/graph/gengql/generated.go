2f020e70e4f8e6fa5e6786f7ea4df194849d78c1
Thomas Krampl <thomas.siegfried.krampl@nav.no>
2024-02-20 16:36:59 +0100

    Update generated code

--- internal/graph/gengql/generated.go
+++ internal/graph/gengql/generated.go
@@ -15284,9 +15284,9 @@ func (ec *executionContext) _GitHubRepository_id(ctx context.Context, field grap
 		}
 		return graphql.Null
 	}
-	res := resTmp.(uuid.UUID)
+	res := resTmp.(scalar.Ident)
 	fc.Result = res
-	return ec.marshalNID2githubᚗcomᚋgoogleᚋuuidᚐUUID(ctx, field.Selections, res)
+	return ec.marshalNID2githubᚗcomᚋnaisᚋapiᚋinternalᚋgraphᚋscalarᚐIdent(ctx, field.Selections, res)
 }
 
 func (ec *executionContext) fieldContext_GitHubRepository_id(ctx context.Context, field graphql.CollectedField) (fc *graphql.FieldContext, err error) {
