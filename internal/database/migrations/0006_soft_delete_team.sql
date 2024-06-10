cc958cad3eb31caf6dd0faa104fe03c1f542c9d8
Kim Tore Jensen <kimtjen@gmail.com>
2024-06-10 14:50:49 +0200

    Bump version of sql migration and make teams into a view

--- internal/database/migrations/0006_soft_delete_team.sql
+++
@@ -1,5 +0,0 @@
--- +goose Up
-
-ALTER TABLE teams ADD deleted_at TIMESTAMPTZ DEFAULT NULL;
-
-CREATE INDEX ON teams (deleted_at);
---
+++ internal/database/migrations/0009_soft_delete_team.sql
@@ -0,0 +1,19 @@
+-- +goose Up
+
+ALTER TABLE teams ADD deleted_at TIMESTAMPTZ DEFAULT NULL;
+
+CREATE INDEX ON teams (deleted_at);
+
+ALTER TABLE teams RENAME TO active_and_deleted_teams;
+
+CREATE VIEW teams AS SELECT * FROM active_and_deleted_teams WHERE deleted_at IS NULL;
+
+-- +goose Down
+
+DROP VIEW teams;
+
+ALTER TABLE active_and_deleted_teams RENAME TO teams;
+
+DELETE FROM teams WHERE deleted_at IS NOT NULL;
+
+ALTER TABLE teams DROP deleted_at;
