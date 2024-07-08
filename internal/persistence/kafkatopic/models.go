162538d2d7ff8dd3daf20bc39b6d98314e6e9e85
Christer Edvartsen <christer.edvartsen@nav.no>
2024-07-08 22:16:03 +0200

    Add some missing fields

--- internal/persistence/kafkatopic/models.go
+++ internal/persistence/kafkatopic/models.go
@@ -2,26 +2,33 @@ package kafkatopic
 
 import (
 	"fmt"
+	"io"
+	"strconv"
+
 	"github.com/nais/api/internal/graphv1/ident"
 	"github.com/nais/api/internal/graphv1/modelv1"
 	"github.com/nais/api/internal/graphv1/pagination"
 	"github.com/nais/api/internal/slug"
 	kafka_nais_io_v1 "github.com/nais/liberator/pkg/apis/kafka.nais.io/v1"
-	"io"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
-	"strconv"
 )
 
 type (
-	KafkaTopicConnection = pagination.Connection[*KafkaTopic]
-	KafkaTopicEdge       = pagination.Edge[*KafkaTopic]
+	KafkaTopicConnection    = pagination.Connection[*KafkaTopic]
+	KafkaTopicEdge          = pagination.Edge[*KafkaTopic]
+	KafkaTopicACLConnection = pagination.Connection[*KafkaTopicACL]
+	KafkaTopicACLEdge       = pagination.Edge[*KafkaTopicACL]
 )
 
 type KafkaTopic struct {
-	Name            string    `json:"name"`
-	TeamSlug        slug.Slug `json:"-"`
-	EnvironmentName string    `json:"-"`
+	Name            string                   `json:"name"`
+	Pool            string                   `json:"pool"`
+	Configuration   *KafkaTopicConfiguration `json:"configuration,omitempty"`
+	Status          KafkaTopicStatus         `json:"status"`
+	ACLs            []*KafkaTopicACL         `json:"-"`
+	TeamSlug        slug.Slug                `json:"-"`
+	EnvironmentName string                   `json:"-"`
 }
 
 func (KafkaTopic) IsPersistence() {}
@@ -73,100 +80,126 @@ func (e KafkaTopicOrderField) MarshalGQL(w io.Writer) {
 	fmt.Fprint(w, strconv.Quote(e.String()))
 }
 
-func toKafkaTopic(u *unstructured.Unstructured, envName string) (*KafkaTopic, error) {
-	kt := &kafka_nais_io_v1.Topic{}
+type KafkaTopicACL struct {
+	Access          string `json:"access"`
+	ApplicationName string `json:"applicationName"`
+	TeamName        string `json:"teamName"`
+	EnvironmentName string `json:"-"`
+}
 
-	if err := runtime.DefaultUnstructuredConverter.FromUnstructured(u.Object, kt); err != nil {
+type KafkaTopicACLOrder struct {
+	Field     KafkaTopicACLOrderField `json:"field"`
+	Direction modelv1.OrderDirection  `json:"direction"`
+}
+
+type KafkaTopicConfiguration struct {
+	CleanupPolicy         *string `json:"cleanupPolicy,omitempty"`
+	MaxMessageBytes       *int    `json:"maxMessageBytes,omitempty"`
+	MinimumInSyncReplicas *int    `json:"minimumInSyncReplicas,omitempty"`
+	Partitions            *int    `json:"partitions,omitempty"`
+	Replication           *int    `json:"replication,omitempty"`
+	RetentionBytes        *int    `json:"retentionBytes,omitempty"`
+	RetentionHours        *int    `json:"retentionHours,omitempty"`
+	SegmentHours          *int    `json:"segmentHours,omitempty"`
+}
+
+type KafkaTopicStatus struct {
+	State string `json:"state"`
+}
+
+type KafkaTopicACLOrderField string
+
+const (
+	KafkaTopicACLOrderFieldTeamSlug KafkaTopicACLOrderField = "TEAM_SLUG"
+	KafkaTopicACLOrderFieldConsumer KafkaTopicACLOrderField = "CONSUMER"
+	KafkaTopicACLOrderFieldAccess   KafkaTopicACLOrderField = "ACCESS"
+)
+
+func (e KafkaTopicACLOrderField) IsValid() bool {
+	switch e {
+	case KafkaTopicACLOrderFieldTeamSlug, KafkaTopicACLOrderFieldConsumer, KafkaTopicACLOrderFieldAccess:
+		return true
+	}
+	return false
+}
+
+func (e KafkaTopicACLOrderField) String() string {
+	return string(e)
+}
+
+func (e *KafkaTopicACLOrderField) UnmarshalGQL(v interface{}) error {
+	str, ok := v.(string)
+	if !ok {
+		return fmt.Errorf("enums must be strings")
+	}
+
+	*e = KafkaTopicACLOrderField(str)
+	if !e.IsValid() {
+		return fmt.Errorf("%s is not a valid KafkaTopicAclOrderField", str)
+	}
+	return nil
+}
+
+func (e KafkaTopicACLOrderField) MarshalGQL(w io.Writer) {
+	fmt.Fprint(w, strconv.Quote(e.String()))
+}
+
+type KafkaTopicACLFilter struct {
+	Team        *slug.Slug `json:"team,omitempty"`
+	Application *string    `json:"application,omitempty"`
+}
+
+func toKafkaTopicConfiguration(cfg *kafka_nais_io_v1.Config) *KafkaTopicConfiguration {
+	if cfg == nil {
+		return nil
+	}
+
+	return &KafkaTopicConfiguration{
+		CleanupPolicy:         cfg.CleanupPolicy,
+		MaxMessageBytes:       cfg.MaxMessageBytes,
+		MinimumInSyncReplicas: cfg.MinimumInSyncReplicas,
+		Partitions:            cfg.Partitions,
+		Replication:           cfg.Replication,
+		RetentionBytes:        cfg.RetentionBytes,
+		RetentionHours:        cfg.RetentionHours,
+		SegmentHours:          cfg.SegmentHours,
+	}
+}
+
+func toKafkaTopicACLs(acls []kafka_nais_io_v1.TopicACL, envName string) []*KafkaTopicACL {
+	ret := make([]*KafkaTopicACL, len(acls))
+	for i, a := range acls {
+		ret[i] = &KafkaTopicACL{
+			Access:          a.Access,
+			ApplicationName: a.Application,
+			TeamName:        a.Team,
+			EnvironmentName: envName,
+		}
+	}
+	return ret
+}
+
+func toKafkaTopicStatus(status *kafka_nais_io_v1.TopicStatus) KafkaTopicStatus {
+	// TODO: Implement
+	return KafkaTopicStatus{}
+}
+
+func toKafkaTopic(u *unstructured.Unstructured, envName string) (*KafkaTopic, error) {
+	obj := &kafka_nais_io_v1.Topic{}
+
+	if err := runtime.DefaultUnstructuredConverter.FromUnstructured(u.Object, obj); err != nil {
 		return nil, fmt.Errorf("converting to KafkaTopic: %w", err)
 	}
 
-	teamSlug := kt.GetNamespace()
+	teamSlug := obj.GetNamespace()
 
 	return &KafkaTopic{
-		Name:            kt.Name,
-		EnvironmentName: envName,
+		Name:            obj.Name,
+		Pool:            obj.Spec.Pool,
+		Configuration:   toKafkaTopicConfiguration(obj.Spec.Config),
+		Status:          toKafkaTopicStatus(obj.Status),
+		ACLs:            toKafkaTopicACLs(obj.Spec.ACL, envName),
 		TeamSlug:        slug.Slug(teamSlug),
-		/*
-			Config: func(cfg *kafka_nais_io_v1.Config) *KafkaTopicConfig {
-				if cfg == nil {
-					return nil
-				}
-				return &KafkaTopicConfig{
-					CleanupPolicy:         cfg.CleanupPolicy,
-					MaxMessageBytes:       cfg.MaxMessageBytes,
-					MinimumInSyncReplicas: cfg.MinimumInSyncReplicas,
-					Partitions:            cfg.Partitions,
-					Replication:           cfg.Replication,
-					RetentionBytes:        cfg.RetentionBytes,
-					RetentionHours:        cfg.RetentionHours,
-					SegmentHours:          cfg.SegmentHours,
-				}
-			}(kt.Spec.Config),
-
-			ACL: func(as []kafka_nais_io_v1.TopicACL) []*KafkaTopicACL {
-				ret := make([]*KafkaTopicACL, len(as))
-				for i, a := range as {
-					ret[i] = &KafkaTopicACL{
-						Access:          a.Access,
-						ApplicationName: a.Application,
-						TeamName:        a.Team,
-
-						GQLVars: KafkaTopicACLGQLVars{
-							Env: env,
-						},
-					}
-				}
-				return ret
-			}(kt.Spec.ACL),
-			Env: Env{
-				Name: env,
-				Team: teamSlug,
-			},
-			GQLVars: KafkaTopicGQLVars{
-				TeamSlug: slug.Slug(teamSlug),
-			},
-			Status: func(s *kafka_nais_io_v1.TopicStatus) *KafkaTopicStatus {
-				if s == nil {
-					return nil
-				}
-
-				var syncTime, expTime, lastSyncTime *time.Time
-				if t, err := time.Parse(time.RFC3339, s.SynchronizationTime); err == nil {
-					syncTime = &t
-				}
-
-				if t, err := time.Parse(time.RFC3339, s.CredentialsExpiryTime); err == nil {
-					expTime = &t
-				}
-
-				if t, err := time.Parse(time.RFC3339, s.LatestAivenSyncFailure); err == nil {
-					lastSyncTime = &t
-				}
-
-				state := StateUnknown
-				switch s.SynchronizationState {
-				case sync_states.FailedPrepare,
-					sync_states.FailedGenerate,
-					sync_states.FailedSynchronization,
-					sync_states.FailedStatusUpdate,
-					sync_states.Retrying:
-					state = StateNotnais
-				case sync_states.Synchronized,
-					sync_states.RolloutComplete:
-					state = StateNais
-				}
-
-				return &KafkaTopicStatus{
-					FullyQualifiedName:     s.FullyQualifiedName,
-					Message:                s.Message,
-					SynchronizationTime:    syncTime,
-					CredentialsExpiryTime:  expTime,
-					Errors:                 s.Errors,
-					LatestAivenSyncFailure: lastSyncTime,
-					SynchronizationState:   state,
-				}
-			}(kt.Status),
-
-		*/
+		EnvironmentName: envName,
 	}, nil
 }
