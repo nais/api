b12064cc45b142d72866597e536995885e2c56a1
Roger Bjørnstad <roger.bjornstad@nav.no>
2024-08-15 15:30:10 +0200

    feat: Implement input validation for CreateFeedback mutation
    The code changes include implementing input validation for the CreateFeedback mutation. This ensures that the feedback details are required and must not exceed 3000 characters.

--- internal/graph/feedback.resolvers.go
+++ internal/graph/feedback.resolvers.go
@@ -8,6 +8,20 @@ import (
 )
 
 func (r *mutationResolver) CreateFeedback(ctx context.Context, input model.CreateFeedbackInput) (*model.CreateFeedbackResult, error) {
+	if len(input.Details) == 0 {
+		return &model.CreateFeedbackResult{
+			Created: false,
+			Error:   ptr.To("Feedback details are required"),
+		}, nil
+	}
+	r.log.Infof("Creating feedback of length %d", len(input.Details))
+	if len(input.Details) > 3000 {
+		return &model.CreateFeedbackResult{
+			Created: false,
+			Error:   ptr.To("Feedback details must be no more than 3000 characters"),
+		}, nil
+	}
+
 	messageOptions := r.slackClient.GetFeedbackMessageOptions(ctx, r.tenant, input)
 	if _, _, err := r.slackClient.PostFeedbackMessage(messageOptions); err != nil {
 		return &model.CreateFeedbackResult{
