aa7baa138d02bd6e1fd647bda685ad20e411d77a
Christer Edvartsen <christer.edvartsen@nav.no>
2024-08-16 11:25:09 +0200

    Have all queries reuse a single transaction instead of supporting nested transactions

--- internal/v1/databasev1/transaction.go
+++ internal/v1/databasev1/transaction.go
@@ -5,7 +5,6 @@ import (
 
 	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
-	_ "github.com/jackc/pgx/v5/stdlib"
 )
 
 type ctxKey int
@@ -19,22 +18,35 @@ func NewLoaderContext(ctx context.Context, dbConn *pgxpool.Pool) context.Context
 	return context.WithValue(ctx, databaseKey, dbConn)
 }
 
+// Transaction executes a callback function within a transaction. The first call to Transaction starts a new
+// transaction, and subsequent calls will execute the callback function in the same transaction.
 func Transaction(ctx context.Context, callback func(ctx context.Context) error) error {
-	conn := ctx.Value(databaseKey).(*pgxpool.Pool)
-	tx, err := conn.Begin(ctx)
-	if err != nil {
-		return err
-	}
+	transactionStarter := false
 
-	defer tx.Rollback(ctx)
+	tx := TransactionFromContext(ctx)
+	if tx == nil {
+		transactionStarter = true
+		var err error
+		tx, err = ctx.Value(databaseKey).(*pgxpool.Pool).Begin(ctx)
+		if err != nil {
+			return err
+		}
+
+		defer tx.Rollback(ctx)
+	}
 
 	if err := callback(context.WithValue(ctx, databaseTransactionKey, tx)); err != nil {
 		return err
 	}
 
+	if !transactionStarter {
+		return nil
+	}
+
 	return tx.Commit(ctx)
 }
 
+// TransactionFromContext will return a potentially open transaction from the context, nil if none exists.
 func TransactionFromContext(ctx context.Context) pgx.Tx {
 	tx, _ := ctx.Value(databaseTransactionKey).(pgx.Tx)
 	return tx
