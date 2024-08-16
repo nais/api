c31122579cad10b5a9612de2dc0fbbd011119a98
Trong Huu Nguyen <trong.huu.nguyen@nav.no>
2024-08-16 13:26:34 +0200

    fix: correct migration for audit action

---
+++ internal/database/migrations/0017_update_audit_events_actions.sql
@@ -0,0 +1,27 @@
+-- +goose Up
+
+UPDATE audit_events
+SET action = 'ADDED'
+WHERE
+    action = 'TEAM_MEMBER_ADDED'
+    AND resource_type = 'TEAM_MEMBER';
+
+UPDATE audit_events
+SET action = 'REMOVED'
+WHERE
+    action = 'TEAM_MEMBER_REMOVED'
+    AND resource_type = 'TEAM_MEMBER';
+
+-- +goose Down
+
+UPDATE audit_events
+SET action = 'TEAM_MEMBER_ADDED'
+WHERE
+    action = 'ADDED'
+    AND resource_type = 'TEAM_MEMBER';
+
+UPDATE audit_events
+SET action = 'TEAM_MEMBER_REMOVED'
+WHERE
+    action = 'REMOVED'
+    AND resource_type = 'TEAM_MEMBER';
