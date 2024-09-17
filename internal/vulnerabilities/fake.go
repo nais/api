ee6049707282fe610e31d4d746ff04e4d8554505
sindrerh2 <sindre.rodseth.hansen@nav.no>
2024-09-17 13:44:34 +0200

    fix: more realistic testdata
    Co-authored-by: tommytroen <tommy.troen@nav.no>
    Co-authored-by: ybelmekk <youssef.bel.mekki@nav.no>

--- internal/vulnerabilities/fake.go
+++ internal/vulnerabilities/fake.go
@@ -3,10 +3,13 @@ package vulnerabilities
 import (
 	"context"
 	"encoding/json"
+	"fmt"
+	"github.com/google/uuid"
 	"github.com/nais/dependencytrack/pkg/client"
 	log "github.com/sirupsen/logrus"
 	"net/url"
 	"os"
+	"strings"
 )
 
 func NewFakeDependencyTrackClient(c client.Client) client.Client {
@@ -36,12 +39,40 @@ func (f *fakeDependencyTrackClient) GetProjectsByTag(ctx context.Context, tag st
 	if err != nil {
 		return nil, err
 	}
-	for _, p := range f.projects {
-		for _, t := range p.Tags {
-			if t.Name == value {
-				projects = append(projects, p)
-			}
+
+	team := strings.Split(value, ":")[1]
+	for i := range 6 {
+		p := &client.Project{
+			Name:    fmt.Sprintf("nais-deploy-chicken-%d", i+2),
+			Tags:    make([]client.Tag, 0),
+			Uuid:    uuid.New().String(),
+			Version: uuid.New().String(),
+			Metrics: &client.ProjectMetric{
+				Critical:           i,
+				High:               i * 2,
+				Medium:             i + 2,
+				Low:                i + 1,
+				Unassigned:         i,
+				FindingsTotal:      i + (i * 2) + (i + 2) + (i + 1) + i,
+				InheritedRiskScore: float64(i*10 + (i*2)*5 + (i+2)*3 + (i + 1) + i*5),
+				Components:         i + 1,
+			},
+			LastBomImport: 1,
 		}
+
+		workloadType := "app"
+		if i%2 != 0 {
+			workloadType = "job"
+		}
+		p.Tags = append(p.Tags, client.Tag{Name: "team:" + team})
+		p.Tags = append(p.Tags, client.Tag{Name: "project:" + p.Name})
+		p.Tags = append(p.Tags, client.Tag{Name: "image:" + p.Name + ":" + p.Version})
+		p.Tags = append(p.Tags, client.Tag{Name: fmt.Sprintf("workload:%s|%s|%s|%s", "dev", team, workloadType, p.Name)})
+		p.Tags = append(p.Tags, client.Tag{Name: "env:" + "dev"})
+		p.Tags = append(p.Tags, client.Tag{Name: fmt.Sprintf("workload:%s|%s|%s|%s", "superprod", team, workloadType, p.Name)})
+		p.Tags = append(p.Tags, client.Tag{Name: "env:" + "superprod"})
+
+		projects = append(projects, p)
 	}
 
 	return projects, nil
