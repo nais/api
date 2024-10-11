b531248012592fe2282816f6e063167cbfd5ef79
Christer Edvartsen <christer.edvartsen@nav.no>
2024-10-11 15:43:39 +0200

    Convert test

---
+++ integration_tests/update_team.lua
@@ -0,0 +1,101 @@
+TeamSlug = "someteamname"
+
+Test.gql("Create team", function(t)
+	t.query(string.format([[
+		mutation {
+			createTeam(
+				input: {
+					slug: "%s"
+					purpose: "some purpose"
+					slackChannel: "#channel"
+				}
+			) {
+				team {
+					slug
+				}
+			}
+		}
+	]], TeamSlug))
+
+	t.check {
+		data = {
+			createTeam = {
+				team = {
+					slug = TeamSlug
+				}
+			}
+		}
+	}
+end)
+
+Test.gql("Update team", function(t)
+	t.query(string.format([[
+		mutation {
+			updateTeam(
+				input: {
+					slug: "%s"
+					purpose: "new-purpose"
+					slackChannel: "#new-slack-channel"
+				}
+			) {
+				team {
+					purpose
+					slackChannel
+					environments {
+						name
+						slackAlertsChannel
+					}
+				}
+			}
+		}
+	]], TeamSlug))
+
+	t.check {
+		data = {
+			updateTeam = {
+				team = {
+					purpose = "new-purpose",
+					slackChannel = "#new-slack-channel",
+					environments = {
+						{
+							name = "dev",
+							slackAlertsChannel = "#new-slack-channel"
+						},
+						{
+							name = "staging",
+							slackAlertsChannel = "#new-slack-channel"
+						}
+					}
+				}
+			}
+		}
+	}
+end)
+
+Test.gql("Nothing to update", function(t)
+	t.query(string.format([[
+		mutation {
+			updateTeam(
+				input: {
+					slug: "%s"
+				}
+			) {
+				team {
+					purpose
+				}
+			}
+		}
+	]], TeamSlug))
+
+	t.check {
+		data = Null,
+		errors = {
+			{
+				message = "Nothing to update.",
+				path = {
+					"updateTeam"
+				}
+			}
+		}
+	}
+end)
