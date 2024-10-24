3666979e3743c11fde3e673a99d4faca48c5f0e1
Thomas Krampl <thomas.siegfried.krampl@nav.no>
2024-10-24 13:22:17 +0200

    add vulnerability id

--- internal/v1/graphv1/gengqlv1/generated.go
+++ internal/v1/graphv1/gengqlv1/generated.go
@@ -431,7 +431,6 @@ type ComplexityRoot struct {
 		Package     func(childComplexity int) int
 		Severity    func(childComplexity int) int
 		State       func(childComplexity int) int
-		URL         func(childComplexity int) int
 	}
 
 	ImageVulnerabilityConnection struct {
@@ -3031,13 +3030,6 @@ func (e *executableSchema) Complexity(typeName, field string, childComplexity in
 		}
 
 		return e.complexity.ImageVulnerability.State(childComplexity), true
-
-	case "ImageVulnerability.url":
-		if e.complexity.ImageVulnerability.URL == nil {
-			break
-		}
-
-		return e.complexity.ImageVulnerability.URL(childComplexity), true
 
 	case "ImageVulnerabilityConnection.edges":
 		if e.complexity.ImageVulnerabilityConnection.Edges == nil {
@@ -11881,9 +11873,6 @@ type ImageVulnerability implements Node {
 
 	"State of the vulnerability."
 	state: ImageVulnerabilityState!
-
-	"External URL to the vulnerability."
-	url: String!
 }
 
 enum ImageVulnerabilitySeverity {
@@ -11913,7 +11902,7 @@ input ImageVulnerabilityOrder {
 enum ImageVulnerabilityOrderField {
 	IDENTIFIER
 	SEVERITY
-	PACKAGE_URL
+	PACKAGE
 	STATE
 }
 
@@ -26996,7 +26985,7 @@ func (ec *executionContext) _ImageVulnerability_id(ctx context.Context, field gr
 	}()
 	resTmp, err := ec.ResolverMiddleware(ctx, func(rctx context.Context) (interface{}, error) {
 		ctx = rctx // use context from middleware stack in children
-		return obj.ID, nil
+		return obj.ID(), nil
 	})
 	if err != nil {
 		ec.Error(ctx, err)
@@ -27017,7 +27006,7 @@ func (ec *executionContext) fieldContext_ImageVulnerability_id(_ context.Context
 	fc = &graphql.FieldContext{
 		Object:     "ImageVulnerability",
 		Field:      field,
-		IsMethod:   false,
+		IsMethod:   true,
 		IsResolver: false,
 		Child: func(ctx context.Context, field graphql.CollectedField) (*graphql.FieldContext, error) {
 			return nil, errors.New("field of type ID does not have child fields")
@@ -27246,50 +27235,6 @@ func (ec *executionContext) fieldContext_ImageVulnerability_state(_ context.Cont
 	return fc, nil
 }
 
-func (ec *executionContext) _ImageVulnerability_url(ctx context.Context, field graphql.CollectedField, obj *vulnerability.ImageVulnerability) (ret graphql.Marshaler) {
-	fc, err := ec.fieldContext_ImageVulnerability_url(ctx, field)
-	if err != nil {
-		return graphql.Null
-	}
-	ctx = graphql.WithFieldContext(ctx, fc)
-	defer func() {
-		if r := recover(); r != nil {
-			ec.Error(ctx, ec.Recover(ctx, r))
-			ret = graphql.Null
-		}
-	}()
-	resTmp, err := ec.ResolverMiddleware(ctx, func(rctx context.Context) (interface{}, error) {
-		ctx = rctx // use context from middleware stack in children
-		return obj.URL, nil
-	})
-	if err != nil {
-		ec.Error(ctx, err)
-		return graphql.Null
-	}
-	if resTmp == nil {
-		if !graphql.HasFieldError(ctx, fc) {
-			ec.Errorf(ctx, "must not be null")
-		}
-		return graphql.Null
-	}
-	res := resTmp.(string)
-	fc.Result = res
-	return ec.marshalNString2string(ctx, field.Selections, res)
-}
-
-func (ec *executionContext) fieldContext_ImageVulnerability_url(_ context.Context, field graphql.CollectedField) (fc *graphql.FieldContext, err error) {
-	fc = &graphql.FieldContext{
-		Object:     "ImageVulnerability",
-		Field:      field,
-		IsMethod:   false,
-		IsResolver: false,
-		Child: func(ctx context.Context, field graphql.CollectedField) (*graphql.FieldContext, error) {
-			return nil, errors.New("field of type String does not have child fields")
-		},
-	}
-	return fc, nil
-}
-
 func (ec *executionContext) _ImageVulnerabilityConnection_pageInfo(ctx context.Context, field graphql.CollectedField, obj *pagination.Connection[*vulnerability.ImageVulnerability]) (ret graphql.Marshaler) {
 	fc, err := ec.fieldContext_ImageVulnerabilityConnection_pageInfo(ctx, field)
 	if err != nil {
@@ -27451,8 +27396,6 @@ func (ec *executionContext) fieldContext_ImageVulnerabilityConnection_nodes(_ co
 				return ec.fieldContext_ImageVulnerability_package(ctx, field)
 			case "state":
 				return ec.fieldContext_ImageVulnerability_state(ctx, field)
-			case "url":
-				return ec.fieldContext_ImageVulnerability_url(ctx, field)
 			}
 			return nil, fmt.Errorf("no field named %q was found under type ImageVulnerability", field.Name)
 		},
@@ -27555,8 +27498,6 @@ func (ec *executionContext) fieldContext_ImageVulnerabilityEdge_node(_ context.C
 				return ec.fieldContext_ImageVulnerability_package(ctx, field)
 			case "state":
 				return ec.fieldContext_ImageVulnerability_state(ctx, field)
-			case "url":
-				return ec.fieldContext_ImageVulnerability_url(ctx, field)
 			}
 			return nil, fmt.Errorf("no field named %q was found under type ImageVulnerability", field.Name)
 		},
@@ -67784,11 +67725,6 @@ func (ec *executionContext) _ImageVulnerability(ctx context.Context, sel ast.Sel
 			}
 		case "state":
 			out.Values[i] = ec._ImageVulnerability_state(ctx, field, obj)
-			if out.Values[i] == graphql.Null {
-				out.Invalids++
-			}
-		case "url":
-			out.Values[i] = ec._ImageVulnerability_url(ctx, field, obj)
 			if out.Values[i] == graphql.Null {
 				out.Invalids++
 			}
