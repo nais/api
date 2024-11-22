617d0ac369f39f6d86ce739da88f8714d07fa9e0
Thomas Krampl <thomas.siegfried.krampl@nav.no>
2024-11-22 14:57:19 +0100

    reduce data from job

--- internal/workload/job/dataloader.go
+++ internal/workload/job/dataloader.go
@@ -2,10 +2,13 @@ package job
 
 import (
 	"context"
+	"fmt"
+	"slices"
 
 	"github.com/nais/api/internal/kubernetes/watcher"
-	"github.com/nais/liberator/pkg/apis/nais.io/v1"
+	nais_io_v1 "github.com/nais/liberator/pkg/apis/nais.io/v1"
 	batchv1 "k8s.io/api/batch/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 )
 
 type ctxKey int
@@ -26,7 +29,7 @@ func NewWatcher(ctx context.Context, mgr *watcher.Manager) *watcher.Watcher[*nai
 }
 
 func NewRunWatcher(ctx context.Context, mgr *watcher.Manager) *watcher.Watcher[*batchv1.Job] {
-	w := watcher.Watch(mgr, &batchv1.Job{})
+	w := watcher.Watch(mgr, &batchv1.Job{}, watcher.WithTransformer(transformJob))
 	w.Start(ctx)
 	return w
 }
@@ -38,4 +41,62 @@ func fromContext(ctx context.Context) *loaders {
 type loaders struct {
 	jobWatcher *watcher.Watcher[*nais_io_v1.Naisjob]
 	runWatcher *watcher.Watcher[*batchv1.Job]
+}
+
+func transformJob(in any) (any, error) {
+	fieldsToRemove := [][]string{
+		{"spec"},
+		{"metadata", "creationTimestamp"},
+		{"metadata", "generateName"},
+		{"metadata", "ownerReferences"},
+		{"metadata", "annotations"},
+		{"metadata", "managedFields"},
+		{"status", "initContainerStatuses"},
+	}
+	labelsToKeep := []string{
+		"app",
+		"team",
+	}
+
+	job := in.(*unstructured.Unstructured)
+
+	// Getting data to keep
+	containers, _, err := unstructured.NestedSlice(job.Object, "spec", "containers")
+	if err != nil {
+		return nil, err
+	}
+
+	newContainers := []any{}
+	for _, container := range containers {
+		c, ok := container.(map[string]any)
+		if !ok {
+			return nil, fmt.Errorf("container is not a map[string]any")
+		}
+		img, _, err := unstructured.NestedString(c, "image")
+		if err != nil {
+			return nil, err
+		}
+		newContainers = append(newContainers, map[string]any{
+			"name":  c["name"],
+			"image": img,
+		})
+	}
+
+	// Removing fields
+	for _, path := range fieldsToRemove {
+		unstructured.RemoveNestedField(job.Object, path...)
+	}
+
+	labels := job.GetLabels()
+	for k := range labels {
+		if !slices.Contains(labelsToKeep, k) {
+			delete(labels, k)
+		}
+	}
+	job.SetLabels(labels)
+
+	// Adding data back
+	unstructured.SetNestedSlice(job.Object, newContainers, "spec", "containers")
+
+	return job, nil
 }
