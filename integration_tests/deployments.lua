2ddeeecda59a034ca1f8ddfdee935823b15b4948
Thomas Krampl <thomas.siegfried.krampl@nav.no>
2025-01-27 08:54:55 +0100

    Test resources and statuses as well

--- integration_tests/deployments.lua
+++ integration_tests/deployments.lua
@@ -44,25 +44,61 @@ end)
 
 -- Create a few deployments
 for i = 1, 3, 1 do
-	Helper.SQLExec([[
-		INSERT INTO deployments (team_slug,	repository,	environment_name)
-		VALUES ('slug-2', CONCAT('org/repo-', $1::text), 'dev');
+	local row = Helper.SQLQueryRow([[
+		INSERT INTO deployments (team_slug, repository, environment_name)
+		VALUES ('slug-2', CONCAT('org/repo-', $1::text), 'dev')
+		RETURNING id::text
 	]], tostring(i))
+
+	Helper.SQLExec([[
+		INSERT INTO deployment_k8s_resources(deployment_id, "group", version, kind, name, namespace)
+		VALUES ($1, 'grp-1', 'v1', 'kind-1', 'name-1', 'slug-2'),
+		($1, 'grp-2', 'v2', 'kind-2', 'name-2', 'slug-2');
+	]], row.id)
+
+	Helper.SQLExec([[
+		INSERT INTO deployment_statuses (deployment_id, state, message)
+		VALUES ($1, 'success', 'Deployment successful');
+	]], row.id)
 end
 
 Test.gql("team with deployments", function(t)
 	t.query([[
-		query {
+		{
 			team(slug: "slug-2") {
-				deployments {
+				deployments(first: 1, after: "42E5H9") {
 					nodes {
+						id
+						createdAt
+						team {
+							slug
+						}
+						resources {
+							nodes {
+								id
+								kind
+								name
+							}
+						}
+						statuses {
+							nodes {
+								id
+								createdAt
+								state
+								message
+							}
+						}
 						repository
-						environment { name }
+						environment {
+							name
+						}
 					}
 					edges {
 						node {
 							repository
-							environment { name }
+							environment {
+								name
+							}
 						}
 					}
 					pageInfo {
@@ -82,20 +118,33 @@ Test.gql("team with deployments", function(t)
 			team = {
 				deployments = {
 					nodes = {
-						{ repository = "org/repo-3", environment = { name = "dev" } },
-						{ repository = "org/repo-2", environment = { name = "dev" } },
-						{ repository = "org/repo-1", environment = { name = "dev" } },
+						{
+							id = NotNull(),
+							createdAt = NotNull(),
+							resources = {
+								nodes = {
+									{ id = NotNull(), kind = "kind-2", name = "name-2" },
+									{ id = NotNull(), kind = "kind-1", name = "name-1" },
+								},
+							},
+							statuses = {
+								nodes = {
+									{ id = NotNull(), createdAt = NotNull(), state = "SUCCESS", message = "Deployment successful" },
+								},
+							},
+							repository = "org/repo-2",
+							environment = { name = "dev" },
+							team = { slug = "slug-2" },
+						},
 					},
 					edges = {
-						{ node = { repository = "org/repo-3", environment = { name = "dev" } } },
 						{ node = { repository = "org/repo-2", environment = { name = "dev" } } },
-						{ node = { repository = "org/repo-1", environment = { name = "dev" } } },
 					},
 					pageInfo = {
-						hasNextPage = false,
-						hasPreviousPage = false,
-						startCursor = "42E5H9",
-						endCursor = "42E5HB",
+						hasNextPage = true,
+						hasPreviousPage = true,
+						startCursor = "42E5HA",
+						endCursor = "42E5HA",
 						totalCount = 3,
 					},
 				},
