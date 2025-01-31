cc8617cefcd4de38abfa098d991c25537ecc8973
Thomas Krampl <thomas.siegfried.krampl@nav.no>
2025-01-31 12:46:13 +0100

    Integration test for application deployments

--- integration_tests/deployments.lua
+++ integration_tests/deployments.lua
@@ -1,3 +1,5 @@
+Helper.readK8sResources("k8s_resources/simple")
+
 Test.gql("team with no deployments", function(t)
 	t.query([[
 		query {
@@ -52,7 +54,7 @@ for i = 1, 3, 1 do
 
 	Helper.SQLExec([[
 		INSERT INTO deployment_k8s_resources(deployment_id, "group", version, kind, name, namespace)
-		VALUES ($1, 'grp-1', 'v1', 'kind-1', 'name-1', 'slug-2'),
+		VALUES ($1, 'nais.io', 'v1alpha1', 'Application', 'app-name', 'slug-2'),
 		($1, 'grp-2', 'v2', 'kind-2', 'name-2', 'slug-2');
 	]], row.id)
 
@@ -117,8 +119,8 @@ Test.gql("team with deployments", function(t)
 							createdAt = NotNull(),
 							resources = {
 								nodes = {
-									{ id = NotNull(), kind = "kind-2", name = "name-2" },
-									{ id = NotNull(), kind = "kind-1", name = "name-1" },
+									{ id = NotNull(), kind = "kind-2",      name = "name-2" },
+									{ id = NotNull(), kind = "Application", name = "app-name" },
 								},
 							},
 							statuses = {
@@ -140,6 +142,172 @@ Test.gql("team with deployments", function(t)
 						startCursor = "42E5HA",
 						endCursor = "42E5HA",
 						totalCount = 3,
+					},
+				},
+			},
+		},
+	}
+end)
+
+
+Test.gql("application with deployments", function(t)
+	t.query([[
+		{
+			team(slug: "slug-2") {
+				environment(name: "dev") {
+					application(name: "app-name") {
+						deployments(first: 1, after: "42E5H9") {
+							nodes {
+								id
+								createdAt
+								teamSlug
+								resources {
+									nodes {
+										id
+										kind
+										name
+									}
+								}
+								statuses {
+									nodes {
+										id
+										createdAt
+										state
+										message
+									}
+								}
+								repository
+								environmentName
+							}
+							edges {
+								node {
+									repository
+									environmentName
+								}
+							}
+							pageInfo {
+								hasNextPage
+								hasPreviousPage
+								startCursor
+								endCursor
+								totalCount
+							}
+						}
+					}
+				}
+			}
+		}
+	]])
+
+	t.check {
+		data = {
+			team = {
+				environment = {
+					application = {
+						deployments = {
+							nodes = {
+								{
+									id = NotNull(),
+									createdAt = NotNull(),
+									resources = {
+										nodes = {
+											{ id = NotNull(), kind = "kind-2",      name = "name-2" },
+											{ id = NotNull(), kind = "Application", name = "app-name" },
+										},
+									},
+									statuses = {
+										nodes = {
+											{ id = NotNull(), createdAt = NotNull(), state = "SUCCESS", message = "Deployment successful" },
+										},
+									},
+									repository = "org/repo-2",
+									environmentName = "dev",
+									teamSlug = "slug-2",
+								},
+							},
+							edges = {
+								{ node = { repository = "org/repo-2", environmentName = "dev" } },
+							},
+							pageInfo = {
+								hasNextPage = true,
+								hasPreviousPage = true,
+								startCursor = "42E5HA",
+								endCursor = "42E5HA",
+								totalCount = 3,
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+end)
+
+Test.gql("application with no deployments", function(t)
+	t.query([[
+		{
+			team(slug: "slug-1") {
+				environment(name: "dev") {
+					application(name: "app-name") {
+						deployments(first: 1) {
+							nodes {
+								id
+								createdAt
+								teamSlug
+								resources {
+									nodes {
+										id
+										kind
+										name
+									}
+								}
+								statuses {
+									nodes {
+										id
+										createdAt
+										state
+										message
+									}
+								}
+								repository
+								environmentName
+							}
+							edges {
+								node {
+									repository
+									environmentName
+								}
+							}
+							pageInfo {
+								hasNextPage
+								hasPreviousPage
+								startCursor
+								endCursor
+								totalCount
+							}
+						}
+					}
+				}
+			}
+		}
+	]])
+
+	t.check {
+		data = {
+			team = {
+				environment = {
+					application = {
+						deployments = {
+							nodes = {},
+							edges = {},
+							pageInfo = {
+								hasNextPage = false,
+								hasPreviousPage = false,
+								startCursor = Null,
+								endCursor = Null,
+								totalCount = 0,
+							},
+						},
 					},
 				},
 			},
