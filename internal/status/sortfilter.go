2a0c75e337fb828c19de36ff1aa2ae4cfb9c6104
Christer Edvartsen <christer.edvartsen@nav.no>
2025-03-06 15:19:36 +0100

    Add more tie-breakers

--- internal/status/sortfilter.go
+++ internal/status/sortfilter.go
@@ -3,21 +3,41 @@ package status
 import (
 	"context"
 
+	"github.com/nais/api/internal/graph/model"
 	"github.com/nais/api/internal/workload"
 	"github.com/nais/api/internal/workload/application"
 	"github.com/nais/api/internal/workload/job"
+	"k8s.io/utils/ptr"
 )
 
 func init() {
 	application.SortFilter.RegisterConcurrentSort("STATUS", func(ctx context.Context, a *application.Application) int {
 		return int(ForWorkload(ctx, a).State)
+	}, application.SortFilterTieBreaker{
+		Field:     "NAME",
+		Direction: ptr.To(model.OrderDirectionAsc),
+	}, application.SortFilterTieBreaker{
+		Field:     "ENVIRONMENT",
+		Direction: ptr.To(model.OrderDirectionAsc),
 	})
 
 	job.SortFilter.RegisterConcurrentSort("STATUS", func(ctx context.Context, a *job.Job) int {
 		return int(ForWorkload(ctx, a).State)
+	}, job.SortFilterTieBreaker{
+		Field:     "NAME",
+		Direction: ptr.To(model.OrderDirectionAsc),
+	}, job.SortFilterTieBreaker{
+		Field:     "ENVIRONMENT",
+		Direction: ptr.To(model.OrderDirectionAsc),
 	})
 
 	workload.SortFilter.RegisterConcurrentSort("STATUS", func(ctx context.Context, a workload.Workload) int {
 		return int(ForWorkload(ctx, a).State)
+	}, workload.SortFilterTieBreaker{
+		Field:     "NAME",
+		Direction: ptr.To(model.OrderDirectionAsc),
+	}, workload.SortFilterTieBreaker{
+		Field:     "ENVIRONMENT",
+		Direction: ptr.To(model.OrderDirectionAsc),
 	})
 }
