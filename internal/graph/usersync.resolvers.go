8ce27c30b20ca4ee3db89a004089d8c461e11951
Christer Edvartsen <christer.edvartsen@nav.no>
2025-03-27 12:22:25 +0100

    Require global admin when reading user sync logs

--- internal/graph/usersync.resolvers.go
+++ internal/graph/usersync.resolvers.go
@@ -3,11 +3,16 @@ package graph
 import (
 	"context"
 
+	"github.com/nais/api/internal/auth/authz"
 	"github.com/nais/api/internal/graph/pagination"
 	"github.com/nais/api/internal/usersync"
 )
 
 func (r *queryResolver) UserSyncLog(ctx context.Context, first *int, after *pagination.Cursor, last *int, before *pagination.Cursor) (*pagination.Connection[usersync.UserSyncLogEntry], error) {
+	if err := authz.RequireGlobalAdmin(ctx); err != nil {
+		return nil, err
+	}
+
 	page, err := pagination.ParsePage(first, after, last, before)
 	if err != nil {
 		return nil, err
