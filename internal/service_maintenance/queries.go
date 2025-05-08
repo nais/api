1f38d82a57793bf4d893bc14f7129911f18b1e1a
carl hedgren <carl.hedgren@nav.no>
2025-05-08 09:50:56 +0200

    We need to propagate aivenproject for valkeyinstances
    and eventually for opensearch etc too
    
    Co-authored-by: Kyrre Havik <kyrre.havik@nav.no>

--- internal/service_maintenance/queries.go
+++ internal/service_maintenance/queries.go
@@ -2,20 +2,14 @@ package servicemaintenance
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/nais/api/internal/persistence/valkey"
 )
 
 func GetServiceMaintenances(ctx context.Context, valkey valkey.ValkeyInstance) (*ServiceMaintenance, error) {
-	fmt.Printf("\n\n\n\n%v\n\n\n", valkey)
 	key := aivenDataLoaderKey{
-		project:     valkey.EnvironmentName,
+		project:     valkey.AivenProject,
 		serviceName: valkey.Name,
 	}
-	res, err := ctx.Value(loadersKey).(*loaders).maintenanceLoader.Load(ctx, &key)
-	if err != nil {
-		return nil, err
-	}
-	return res, nil
+	return ctx.Value(loadersKey).(*loaders).maintenanceLoader.Load(ctx, &key)
 }
