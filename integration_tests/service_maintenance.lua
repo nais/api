2db1221110d1e2a6142b20ee3d38729ea3a73268
carl hedgren <carl.hedgren@nav.no>
2025-05-12 15:27:26 +0200

    Add a lua integration test
    Co-authored-by: Kyrre Havik <kyrre.havik@nav.no>
    Co-authored-by: x10an14-nav <christian.chavez@nav.no>

--- integration_tests/service_maintenance.lua
+++ integration_tests/service_maintenance.lua
@@ -1,72 +1,67 @@
+Helper.readK8sResources("./k8s_resources/simple")
 local user = User.new()
-local teamSlug = "devteam"
-
-Test.gql("Create team", function(t)
-	t.addHeader("x-user-email", user:email())
-
-	t.query(string.format([[
-		mutation {
-			createTeam(
-				input: {
-					slug: "%s",
-					purpose: "some purpose",
-					slackChannel: "#channel"
-				}
-			) {
-				team {
-					slug
-				}
-			}
-		}
-	]], teamSlug))
-
-	t.check {
-		data = {
-			createTeam = {
-				team = {
-					slug = teamSlug,
-				},
-			},
-		},
-	}
-end)
-
+local team = Team.new("slug-1", "purpose", "#channel")
 
 Test.gql("Show maintenance updates for Valkey", function(t)
     t.addHeader("x-user-email", user:email())
 
-	t.query([[
-        {
-		  team(slug:"devteam") {
-		    valkeyInstances {
-		      edges {
-		        node {
-		        	name
-		          maintenance {
-		            updates {
-		              deadline
-		              title
-		              description
-		              documentation_link
-		              start_at
-		              start_after
-		            }
-		          }
-		        }
-		      }
-		    }
-		  }
-		}
-	]])
+    t.query(string.format([[
+{
+  team(slug: "%s") {
+    valkeyInstances {
+      edges {
+        node {
+       	name
+          project
 
-	t.check {
-		data = {
-			team = {
-				valkeyInstances = {
-					edges = {},
-					nodes = {},
-				},
-			},
-		},
-	}
+        maintenance {
+            updates {
+              description
+              documentation_link
+              start_after
+              start_at
+              deadline
+            }
+          }
+        }
+      }
+    }
+  }
+}	]], team:slug()))
+
+t.check {
+    data = {
+        team = {
+            valkeyInstances = {
+                edges = {
+                    {
+                        node = {
+                            name = "valkey-slug-1-contests",
+                            project = "nav-dev",
+                            maintenance = {
+                                updates = {
+                                    {
+                                        description = "This is the impact (Nais API call it description)",
+                                        documentation_link = "https://nais.io",
+					start_after = Null,
+					start_at = "1987-07-09T00:00:00Z",
+					deadline = Null
+
+                                    },
+                                    {
+                                        description = "This is the impact (Nais API call it description)",
+                                        documentation_link = Null,
+					start_after = "1987-07-09T01:00:00Z",
+					start_at = "1987-07-09T00:00:00Z",
+					deadline = "1987-07-10T00:00:00Z"
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
 end)
