c9e0f0f5d6bc9564a38b751bc785fe6307bf7f2c
Thomas Krampl <thomas.siegfried.krampl@nav.no>
2025-08-06 11:31:15 +0200

    Allow retry without recreating indexes

--- internal/database/migrations/0040_deployment_indexes.sql
+++ internal/database/migrations/0040_deployment_indexes.sql
@@ -1,33 +1,33 @@
 -- +goose NO TRANSACTION
 -- +goose Up
-CREATE INDEX ON deployments (team_slug, created_at DESC)
+CREATE INDEX IF NOT EXISTS deployments_team_slug_created_at_idx ON deployments (team_slug, created_at DESC)
 ;
 
-CREATE INDEX ON deployments (team_slug, environment_name, created_at DESC)
+CREATE INDEX IF NOT EXISTS deployments_team_slug_environment_name_created_at_idx ON deployments (team_slug, environment_name, created_at DESC)
 ;
 
-CREATE INDEX ON deployment_k8s_resources (deployment_id, created_at DESC)
+CREATE INDEX IF NOT EXISTS deployment_k8s_resources_deployment_id_created_at_idx ON deployment_k8s_resources (deployment_id, created_at DESC)
 ;
 
-CREATE INDEX ON deployment_statuses (deployment_id, created_at DESC)
+CREATE INDEX IF NOT EXISTS deployment_statuses_deployment_id_created_at_idx ON deployment_statuses (deployment_id, created_at DESC)
 ;
 
-DROP INDEX IF EXISTS deployments_created_at_idx
+DROP INDEX CONCURRENTLY IF EXISTS deployments_created_at_idx
 ;
 
-DROP INDEX IF EXISTS deployments_team_slug_environment_name_idx
+DROP INDEX CONCURRENTLY IF EXISTS deployments_team_slug_environment_name_idx
 ;
 
-DROP INDEX IF EXISTS deployment_k8s_resources_created_at_idx
+DROP INDEX CONCURRENTLY IF EXISTS deployment_k8s_resources_created_at_idx
 ;
 
-DROP INDEX IF EXISTS deployment_k8s_resources_deployment_id_idx
+DROP INDEX CONCURRENTLY IF EXISTS deployment_k8s_resources_deployment_id_idx
 ;
 
-DROP INDEX IF EXISTS deployment_statuses_created_at_idx
+DROP INDEX CONCURRENTLY IF EXISTS deployment_statuses_created_at_idx
 ;
 
-DROP INDEX IF EXISTS deployment_statuses_deployment_id_idx
+DROP INDEX CONCURRENTLY IF EXISTS deployment_statuses_deployment_id_idx
 ;
 
 VACUUM
