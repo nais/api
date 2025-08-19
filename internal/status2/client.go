c8c914080608e009832cd63b4503bc34230854bf
Johnny Fredheim Horvi <johnny@horvi.no>
2025-08-19 14:27:26 +0200

    Implemented sqlinstances
    Co-authored-by: Tommy Trøen <tommy.troen@nav.no>
    Co-authored-by: frodesundby <frode.sundby@nav.no>

--- internal/status2/client.go
+++ internal/status2/client.go
@@ -6,10 +6,19 @@ import (
 	"time"
 
 	aiven "github.com/aiven/go-client-codegen"
+	"github.com/nais/api/internal/persistence/opensearch"
+	"github.com/nais/api/internal/persistence/sqlinstance"
+	"google.golang.org/api/sqladmin/v1"
 )
 
+var ResourceTypes = []string{
+	"opensearch",
+}
+
 type Client struct {
-	aivenClient aiven.Client
+	aivenClient       aiven.Client
+	sqlInstanceClient *sqladmin.InstancesService
+	info              Info
 }
 
 type Message struct {
@@ -23,11 +32,81 @@ type Severity string
 type Status struct {
 	ResourceType string
 	Name         string
+	Environment  string
+	Team         string
 	Messages     []*Message
 	State        State
 }
 
 type State string
+
+type Info interface {
+	OpenSearches(ctx context.Context) ([]*opensearch.OpenSearch, error)
+	SQLInstances(ctx context.Context) ([]sqlinstance.SQLInstance, error)
+}
+
+func (c *Client) Run(ctx context.Context) ([]*Status, error) {
+	statuses := make([]*Status, 0)
+
+	openSearches, err := c.info.OpenSearches(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get OpenSearches: %w", err)
+	}
+	for _, os := range openSearches {
+		s, err := c.AivenStatus(ctx, os.AivenProject, os.Name, "opensearch")
+		if err != nil {
+			return nil, fmt.Errorf("failed to get Aiven status for OpenSearch %s: %w", os.Name, err)
+		}
+		s.Environment = os.EnvironmentName
+		s.Team = os.TeamSlug.String()
+		statuses = append(statuses, s)
+	}
+
+	sqlInstances, err := c.info.SQLInstances(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get SQL instances: %w", err)
+	}
+	for _, sqlInstance := range sqlInstances {
+		s, err := c.CloudSQLStatus(ctx, sqlInstance.ProjectID, sqlInstance.Name)
+		if err != nil {
+			return nil, fmt.Errorf("failed to get Cloud SQL status for instance %s: %w", sqlInstance.Name, err)
+		}
+		s.Environment = sqlInstance.EnvironmentName
+		s.Team = sqlInstance.TeamSlug.String()
+		statuses = append(statuses, s)
+	}
+
+	return statuses, nil
+}
+
+func (c *Client) CloudSQLStatus(ctx context.Context, project, name string) (*Status, error) {
+	instance, err := c.sqlInstanceClient.Get(project, name).Context(ctx).Do()
+	if err != nil {
+		return nil, fmt.Errorf("failed to get Cloud SQL instance %s: %w", name, err)
+	}
+	ret := &Status{
+		ResourceType: "sqlinstance",
+		Name:         name,
+		State:        sqlStatus(instance),
+	}
+
+	for _, sr := range instance.SuspensionReason {
+		ret.Messages = append(ret.Messages, &Message{
+			Created:  time.Now(),
+			Event:    sr,
+			Severity: Severity("critical"),
+		})
+	}
+
+	return ret, nil
+}
+
+func sqlStatus(instance *sqladmin.DatabaseInstance) State {
+	if instance.Settings.ActivationPolicy == "NEVER" {
+		return State("STOPPED")
+	}
+	return State(instance.State)
+}
 
 func (c *Client) AivenStatus(ctx context.Context, project, name, resourceType string) (*Status, error) {
 	s, err := c.aivenClient.ServiceGet(ctx, project, name)
@@ -53,10 +132,16 @@ func (c *Client) AivenStatus(ctx context.Context, project, name, resourceType st
 	}
 	return ret, nil
 }
-func NewClient(ctx context.Context, token string) (*Client, error) {
+func NewClient(ctx context.Context, token string, info Info) (*Client, error) {
 	c, err := aiven.NewClient(aiven.TokenOpt(token), aiven.UserAgentOpt("nais-api"))
 	if err != nil {
 		return nil, err
 	}
-	return &Client{aivenClient: c}, nil
+
+	admin, err := sqladmin.NewService(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create SQL Admin service: %w", err)
+	}
+
+	return &Client{aivenClient: c, info: info, sqlInstanceClient: admin.Instances}, nil
 }
--- internal/status2/client_test.go
+++ internal/status2/client_test.go
@@ -8,9 +8,47 @@ import (
 
 	"github.com/davecgh/go-spew/spew"
 	"github.com/joho/godotenv"
+	"github.com/nais/api/internal/persistence/opensearch"
+	"github.com/nais/api/internal/persistence/sqlinstance"
 	"github.com/nais/api/internal/status2"
 	"github.com/stretchr/testify/assert"
 )
+
+type FakeInfo struct{}
+
+func (f FakeInfo) OpenSearches(ctx context.Context) ([]*opensearch.OpenSearch, error) {
+	return []*opensearch.OpenSearch{
+		{
+			Name:            "opensearch-tbd-sparsom",
+			AivenProject:    "nav-prod",
+			TeamSlug:        "tbd",
+			EnvironmentName: "prod-gcp",
+		},
+		{
+			Name:            "opensearch-tbd-sparsom",
+			AivenProject:    "nav-dev",
+			TeamSlug:        "tbd",
+			EnvironmentName: "dev-gcp",
+		},
+	}, nil
+}
+
+func (f FakeInfo) SQLInstances(ctx context.Context) ([]sqlinstance.SQLInstance, error) {
+	return []sqlinstance.SQLInstance{
+		{
+			Name:            "mulighetsrommet-api-v1",
+			ProjectID:       "team-mulighetsrommet-dev-a2d7",
+			TeamSlug:        "team-mulighetsrommet",
+			EnvironmentName: "dev-gcp",
+		},
+		{
+			Name:            "spinnvill-clone-for-fremtidig-feilretting",
+			ProjectID:       "tbd-prod-eacd",
+			TeamSlug:        "tbd",
+			EnvironmentName: "prod-gcp",
+		},
+	}, nil
+}
 
 func TestNewClient(t *testing.T) {
 	if err := godotenv.Load("../../.env"); err != nil {
@@ -19,13 +57,12 @@ func TestNewClient(t *testing.T) {
 	}
 	ctx := context.Background()
 	token := os.Getenv("AIVEN_TOKEN")
-	client, err := status2.NewClient(ctx, token)
+	client, err := status2.NewClient(ctx, token, FakeInfo{})
 	assert.NoError(t, err)
 	assert.NotNil(t, client)
 
-	d, err := client.AivenStatus(ctx, "nav-prod", "opensearch-tbd-sparsom", "yolo")
+	statuses, err := client.Run(ctx)
 	assert.NoError(t, err)
-	assert.NotNil(t, d)
-	spew.Dump(d)
 
+	spew.Dump(statuses)
 }
