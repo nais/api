5506faaf8d6896acb673e4e5349d4dbffbaa668b
Thomas Krampl <thomas.siegfried.krampl@nav.no>
2025-08-21 11:12:45 +0200

    Set distinct nulls on cost unique index
    Without nulls generates duplicate rows

---
+++ internal/database/migrations/0045_cost_distinct_nulls.sql
@@ -0,0 +1,18 @@
+-- +goose Up
+
+TRUNCATE TABLE cost;
+
+ALTER TABLE cost
+DROP CONSTRAINT daily_cost_key
+;
+
+ALTER TABLE cost
+ADD CONSTRAINT daily_cost_key UNIQUE NULLS NOT DISTINCT (
+	environment,
+	team_slug,
+	app_label,
+	service,
+	date,
+	service_name
+)
+;
