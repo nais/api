0a4bbd08da3b73efeb2543bc90af968de8ae9cb8
Vegar Sechmann Molvig <vegar.sechmann.molvig@nav.no>
2025-10-10 13:24:15 +0200

    fix: more specific types in graph, default

--- internal/graph/log.resolvers.go
+++ internal/graph/log.resolvers.go
@@ -2,10 +2,23 @@ package graph
 
 import (
 	"context"
+	"fmt"
 
+	"github.com/nais/api/internal/graph/gengql"
+	"github.com/nais/api/internal/graph/model/donotuse"
 	"github.com/nais/api/internal/loki"
 )
 
 func (r *subscriptionResolver) Log(ctx context.Context, filter loki.LogSubscriptionFilter) (<-chan *loki.LogLine, error) {
 	return loki.LogStream(ctx, &filter)
 }
+
+func (r *logSubscriptionFilterResolver) InitialBatch(ctx context.Context, obj *loki.LogSubscriptionFilter, data *donotuse.LogSubscriptionInitialBatch) error {
+	panic(fmt.Errorf("not implemented: InitialBatch - initialBatch"))
+}
+
+func (r *Resolver) LogSubscriptionFilter() gengql.LogSubscriptionFilterResolver {
+	return &logSubscriptionFilterResolver{r}
+}
+
+type logSubscriptionFilterResolver struct{ *Resolver }
