2a4669cb9aa0d936755dc8b647ea65cf2764e3f6
Vegar Sechmann Molvig <vegar.sechmann.molvig@nav.no>
2025-10-10 15:00:19 +0200

    fix: rename field in graphql schema as well

--- internal/graph/log.resolvers.go
+++ internal/graph/log.resolvers.go
@@ -2,22 +2,10 @@ package graph
 
 import (
 	"context"
-	"fmt"
 
-	"github.com/nais/api/internal/graph/gengql"
 	"github.com/nais/api/internal/loki"
 )
 
 func (r *subscriptionResolver) Log(ctx context.Context, filter loki.LogSubscriptionFilter) (<-chan *loki.LogLine, error) {
 	return loki.LogStream(ctx, &filter)
 }
-
-func (r *logSubscriptionFilterResolver) InitialBatch(ctx context.Context, obj *loki.LogSubscriptionFilter, data *loki.LogSubscriptionInitialBatch) error {
-	panic(fmt.Errorf("not implemented: InitialBatch - initialBatch"))
-}
-
-func (r *Resolver) LogSubscriptionFilter() gengql.LogSubscriptionFilterResolver {
-	return &logSubscriptionFilterResolver{r}
-}
-
-type logSubscriptionFilterResolver struct{ *Resolver }
