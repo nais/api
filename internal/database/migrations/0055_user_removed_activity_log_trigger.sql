040bda05d7351633fd89560a61a007933d42e2ac
Thomas Krampl <thomas.siegfried.krampl@nav.no>
2025-11-21 15:20:21 +0100

    fmt

--- internal/database/migrations/0055_user_removed_activity_log_trigger.sql
+++ internal/database/migrations/0055_user_removed_activity_log_trigger.sql
@@ -2,10 +2,8 @@
 -- Add trigger to create activity log when a user is deleted (e.g. by usersync)
 -- When the user is deleted, we should get all user_roles for the user where a team_slug is not null
 -- and create an activity log for each team the user was a member of
-
 -- +goose StatementBegin
-CREATE OR REPLACE FUNCTION log_user_deletion()
-RETURNS TRIGGER AS $$
+CREATE OR REPLACE FUNCTION log_user_deletion () RETURNS TRIGGER AS $$
 BEGIN
 		INSERT INTO activity_log_entries (actor, action, resource_type, resource_name, team_slug, data, environment)
 		SELECT DISTINCT
@@ -24,11 +22,13 @@ BEGIN
 
 		RETURN OLD;
 END;
-$$ LANGUAGE plpgsql;
--- +goose StatementEnd
+$$ LANGUAGE plpgsql
+;
 
-DROP TRIGGER IF EXISTS user_deletion_trigger ON users;
-CREATE TRIGGER user_deletion_trigger
-BEFORE DELETE ON users
-FOR EACH ROW
-EXECUTE FUNCTION log_user_deletion();
+-- +goose StatementEnd
+DROP TRIGGER IF EXISTS user_deletion_trigger ON users
+;
+
+CREATE TRIGGER user_deletion_trigger BEFORE DELETE ON users FOR EACH ROW
+EXECUTE FUNCTION log_user_deletion ()
+;
