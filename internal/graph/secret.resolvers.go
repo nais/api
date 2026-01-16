ce0275aae8e366cfe8810ea1d7036178ecc3d915
frodesundby <frode.sundby@nav.no>
2026-01-16 15:33:12 +0100

    Remove obsolete commented code from secret resolvers

--- internal/graph/secret.resolvers.go
+++ internal/graph/secret.resolvers.go
@@ -234,15 +234,3 @@ func (r *teamEnvironmentResolver) Secret(ctx context.Context, obj *team.TeamEnvi
 func (r *Resolver) Secret() gengql.SecretResolver { return &secretResolver{r} }
 
 type secretResolver struct{ *Resolver }
-
-// !!! WARNING !!!
-// The code below was going to be deleted when updating resolvers. It has been copied here so you have
-// one last chance to move it out of harms way if you want. There are two reasons this happens:
-//  - When renaming or deleting a resolver the old code will be put in here. You can safely delete
-//    it when you're done.
-//  - You have helper methods in this file. Move them out to keep these resolver files clean.
-/*
-	func (r *secretResolver) Keys(ctx context.Context, obj *secret.Secret) ([]string, error) {
-	return obj.Keys, nil
-}
-*/
