009cf099a33a700a75a34c285013219029dcb9ac
Thomas Krampl <thomas.siegfried.krampl@nav.no>
2026-02-11 14:18:58 +0100

    Fix missing fix from other branch

--- internal/persistence/valkey/queries.go
+++ internal/persistence/valkey/queries.go
@@ -173,6 +173,9 @@ func Create(ctx context.Context, input CreateValkeyInput) (*CreateValkeyPayload,
 		EnvironmentName: ptr.To(input.EnvironmentName),
 		TeamSlug:        ptr.To(input.TeamSlug),
 	})
+	if err != nil {
+		return nil, err
+	}
 
 	obj, err := kubernetes.ToUnstructured(res)
 	if err != nil {
