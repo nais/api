c936698adf175524c5c75b1352c621a760c9c364
Johnny Fredheim Horvi <johnny@horvi.no>
2026-03-16 09:00:05 +0100

    Fix lint: apply prettier formatting to SQL and GraphQL schema files

--- internal/database/migrations/0059_add_aiven_credentials_create_authorization.sql
+++ internal/database/migrations/0059_add_aiven_credentials_create_authorization.sql
@@ -1,29 +1,27 @@
 -- +goose Up
 INSERT INTO
-    authorizations (name, description)
+	authorizations (name, description)
 VALUES
-    (
-        'aiven:credentials:create',
-        'Permission to create Aiven service credentials.'
-    )
+	(
+		'aiven:credentials:create',
+		'Permission to create Aiven service credentials.'
+	)
 ;
 
 INSERT INTO
-    role_authorizations (role_name, authorization_name)
+	role_authorizations (role_name, authorization_name)
 VALUES
-    ('Team member', 'aiven:credentials:create'),
-    ('Team owner', 'aiven:credentials:create')
+	('Team member', 'aiven:credentials:create'),
+	('Team owner', 'aiven:credentials:create')
 ;
 
 -- +goose Down
-DELETE FROM
-    role_authorizations
+DELETE FROM role_authorizations
 WHERE
-    authorization_name = 'aiven:credentials:create'
+	authorization_name = 'aiven:credentials:create'
 ;
 
-DELETE FROM
-    authorizations
+DELETE FROM authorizations
 WHERE
-    name = 'aiven:credentials:create'
+	name = 'aiven:credentials:create'
 ;
