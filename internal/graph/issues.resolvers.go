c9687480c4deaacab585efc9e7f9ba67a678b69a
Frode Sundby <frode.sundby@nav.no>
2026-04-16 14:25:20 +0200

    generate and fmt

--- internal/graph/issues.resolvers.go
+++ internal/graph/issues.resolvers.go
@@ -221,18 +221,3 @@ type (
 	valkeyIssueResolver                               struct{ *Resolver }
 	vulnerableImageIssueResolver                      struct{ *Resolver }
 )
-
-// !!! WARNING !!!
-// The code below was going to be deleted when updating resolvers. It has been copied here so you have
-// one last chance to move it out of harms way if you want. There are two reasons this happens:
-//  - When renaming or deleting a resolver the old code will be put in here. You can safely delete
-//    it when you're done.
-//  - You have helper methods in this file. Move them out to keep these resolver files clean.
-/*
-	func (r *restartLoopIssueResolver) TeamEnvironment(ctx context.Context, obj *issue.ApplicationRestartLoopIssue) (*team.TeamEnvironment, error) {
-	return team.GetTeamEnvironment(ctx, obj.TeamSlug, obj.EnvironmentName)
-}
-func (r *restartLoopIssueResolver) Workload(ctx context.Context, obj *issue.ApplicationRestartLoopIssue) (workload.Workload, error) {
-	return getWorkloadByResourceType(ctx, obj.TeamSlug, obj.EnvironmentName, obj.ResourceName, obj.ResourceType)
-}
-*/
