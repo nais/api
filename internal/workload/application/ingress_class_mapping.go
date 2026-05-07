b9f2f339a54898bcf102f14629ee1e674f6a3304
Roger Bjørnstad <roger.bjornstad@nav.no>
2026-05-07 15:03:32 +0200

    Add haproxy ingress classes to ingress class mapping
    Adds internal-haproxy, external-haproxy, and external-fa-haproxy to
    the ingress class mapping so that ingresses using these classes are
    correctly identified instead of showing as UNKNOWN.

--- internal/workload/application/ingress_class_mapping.go
+++ internal/workload/application/ingress_class_mapping.go
@@ -4,4 +4,7 @@ var ingressClassMapping = map[string]IngressType{
 	"nais-ingress":          IngressTypeInternal,
 	"nais-ingress-external": IngressTypeExternal,
 	"nais-ingress-fa":       IngressTypeAuthenticated,
+	"internal-haproxy":      IngressTypeInternal,
+	"external-haproxy":      IngressTypeExternal,
+	"external-fa-haproxy":   IngressTypeAuthenticated,
 }
